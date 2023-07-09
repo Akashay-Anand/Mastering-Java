4ccd4d49475562273b115418c678ed07fd7d8437
Akashay Anand <akashayanand1@gmail.com>
2023-07-09 23:58:58 +0530

    more on pojo

--- src/OOPs/J00_POJO.java
+++ src/OOPs/J00_POJO.java
@@ -1,5 +1,17 @@
 package OOPs;
 
+// Documentation for POJO
+/*
+ *> A plain old Java object (whose acronym is POJO) is a class that generally only has instance fields.
+ *
+ *> It usually has few, if any methods other than getters and setters.
+
+ *> Many database frameworks use POJO's to read data from, or to write data to, databases, files or streams.
+
+ * 
+ */
+
+
 public class J00_POJO {
     
 }
@@ -8,3 +20,29 @@ public class J00_POJO {
 record hello(){ 
 
 }
+
+
+// Exploring Java Bean vs POJO vs Spring Bean
+/*
+ * . Java Bean: Classes adhering to 3 constraints:
+        > 1: Have public default (no argument) constructors
+        > 2: Allow access to their properties using getter and setter methods
+        > 3: lmplementjava.io.Serializable
+ *
+ * . POJO: Plain Old Java Object
+        > No coxnstraints
+        > Any Java Object is a POJO!
+ * 
+ * . Spring Bean: Any Java object that is managed by Spring
+        > Spring uses IOC Container (Bean Factory or Application Context) to manage these objects
+ * 
+ */
+
+ /*
+  * When user send the request to the server via HTTP protocol to get the particular information
+then the client request reach to the server and server find the object related to the request. The
+data available in the form of Object at server is called POJO.
+
+ *
+ * 
+  */
